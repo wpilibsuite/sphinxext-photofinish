2b1d4cc21187f449467f482c6b32a82bcf094438
Daltz333 <daltzsmith@gmail.com>
2021-10-04 23:33:02 -0400

    Black

--- sphinxext/photofinish/svgtopng.py
+++ sphinxext/photofinish/svgtopng.py
@@ -350,7 +350,7 @@ def svg_to_png(
         if not SVG_NO_TOOL_MSG_PRINTED:
             logger.info(
                 "No conversion tool was found. Please install one of: cairosvg, svglib, inkscape, rsvg-convert, svgexport, or imagemagick. If you have one of these installed, they may not be on your path. Pass your installed tool's path into this function."
-            ) 
+            )
             SVG_NO_TOOL_MSG_PRINTED = True
 
         raise NoToolError("\n".join(err_msg))
