1c37e431178f0f17f6f08cfd3c3c610c2a06b669
Dalton Smith <darasmith@wccnet.edu>
2021-10-06 11:37:04 -0400

    move global declarations (#6)

--- sphinxext/photofinish/__init__.py
+++ sphinxext/photofinish/__init__.py
@@ -41,7 +41,6 @@ from .svgtopng import NoToolError, svg_to_png
 
 READTHEDOCS_BUILDERS = ["readthedocs", "readthedocsdirhtml"]
 SVG_NO_TOOL_MSG_PRINTED = False
-
 logger = logging.getLogger(__name__)
 
 
@@ -127,6 +126,8 @@ def visit_image(
     if "decoding" not in soup_img.attrs:
         soup_img.attrs["decoding"] = "async"
 
+    global SVG_NO_TOOL_MSG_PRINTED
+
     # SVG handling
     # For svgs, we don't care about generating various codecs / resolutions.
     # But, we do care about having accurate lazyloading placeholders to prevent browser layout shift.
@@ -143,10 +144,8 @@ def visit_image(
                 soup_img.attrs["height"] = im_height
                 soup_img.attrs["width"] = im_width
             except NoToolError as e:
-                global SVG_NO_TOOL_MSG_PRINTED
                 if not SVG_NO_TOOL_MSG_PRINTED:
                     print(e)
-                    global SVG_NO_TOOL_MSG_PRINTED
                     SVG_NO_TOOL_MSG_PRINTED = True
             except Exception as e:
                 print(
